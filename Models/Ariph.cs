3362f0f2c51544f363646aba22f78c4998cc7fd7
Alexei.andriyanov@gmail.com <Alexei.andriyanov@gmail.com>
2021-12-29 19:50:45 +0300

    Добавьте файлы проекта.

---
+++ Models/Ariph.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2_19.Models
+{
+    static class Ariph
+    {
+        public static int Add(int a, int b) => (int)(2*Math.PI*a);
+    }
+}
---
+++ ViewModels/CallerMemberNameAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Lab_2_19.ViewModels
+{
+    internal class CallerMemberNameAttribute : Attribute
+    {
+    }
+}
